e567038655acfff405b13586bd52d20bc89498ba
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2018-03-16 21:28:40 +0300

    -with link issue

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,30 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "BattleTank.h"
+#include "TankBarrel.h"
 #include "TankAimingComponent.h"
-#include "Kismet/GameplayStatics.h"
-#include "Components/StaticMeshComponent.h"
 
+#include "Kismet/GameplayStatics.h"
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
 }
 
-void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
+void UTankAimingComponent::SetBarrelReference(UTankBarrel* BarrelToSet)
 {
 	Barrel = BarrelToSet;
 }
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	if (! Barrel) {	return;	}
+	if (!Barrel) { return; }
 
 	FVector OutLaunchVelocity;
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
-
-	bool bHaveAimSolution =UGameplayStatics::SuggestProjectileVelocity(
+	bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity
+	(
 		this,
 		OutLaunchVelocity,
 		StartLocation,
@@ -32,44 +36,22 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 		LaunchSpeed,
 		ESuggestProjVelocityTraceOption::DoNotTrace
 	);
-
 	if (bHaveAimSolution)
 	{
-		auto TankName = GetOwner()->GetName();
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-		UE_LOG(
-			LogTemp,
-			Warning,
-			TEXT("%s Aiming at %s"),
-			*TankName,
-			*AimDirection.ToString()
-		);
-		// Move barrel
-		// get project to plane 
-		// move barrel horizontzly
-		// get project vertical - and vertical movement
-
+		MoveBarrelTowards(AimDirection);
 	}
+	// If no solution found do nothing
 }
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
-	// Work-out difference between current barrel rotation, and AimDiretion
+	// Work-out difference between current barrel roation, and AimDirection
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
-	UE_LOG(
-			LogTemp,
-			Warning,
-			TEXT("AimAsRotator at %s"),
-			*AimAsRotator.ToString()
-	);
-
-	// Move the barrel the right amount this frame
-
-	// Geven a max elevation speed. and the frame time
-
+	Barrel->Elevate(5); // TODO remove magic number
 }
 
 
