2028878278de0554c39e8f83bdb4c1dac4e00c62
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2018-03-17 00:08:07 +0300

    -missed barrel class added

---
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankBarrel.h"
+
+
+void Elevate(float DegreesPerSecond)
+{
+	// Move the barrel the right amount this frame
+
+	// Geven a max elevation speed. and the frame time
+
+	UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("Elevate for %f"),
+			DegreesPerSecond
+	);
+}
+
